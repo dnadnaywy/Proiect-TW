531e559c2d6ba2264af6a9aa327b3d9fc52cd6bb
brianamaftei <briana.maftei@gmail.com>
2023-04-08 21:09:43 +0300

    js sidebar sub header

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -10,12 +10,12 @@ searchBtn.addEventListener("click", () => {
   sidebar.classList.toggle("open");
 });
 
-let header = document.querySelector(".header");
-let mobileButton = document.querySelector("#mobile-btn");
+// let header = document.querySelector(".header");
+// let mobileButton = document.querySelector("#mobile-btn");
 
-mobileButton.addEventListener("click", () => {
-  header.classList.toggle("open");
-});
+// mobileButton.addEventListener("click", () => {
+//   header.classList.toggle("open");
+// });
 
 // following are the code to change sidebar button(optional)
 /*function menuBtnChange() {
@@ -25,3 +25,47 @@ mobileButton.addEventListener("click", () => {
     closeBtn.classList.replace("bx-menu-alt-right", "bx-menu");//replacing the icons class
   }
 }*/
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hHeader = document.querySelector('.header');
+  const headerReact = hHeader.getBoundingClientRect();
+
+  class getHeightHeader {
+    constructor() {
+      this.height = `${headerReact.height}px`;
+    }
+  }
+
+  const heightHeader = new getHeightHeader();
+
+  function getHeightH() {
+    console.log(heightHeader.height);
+  }
+
+  getHeightH();
+
+  const root=document.documentElement;
+  root.style.setProperty('--top', heightHeader.height);
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const hHeader = document.querySelector('.header-header');
+  const headerReact = hHeader.getBoundingClientRect();
+
+  class getHeightHeader {
+    constructor() {
+      this.height = `${headerReact.height}px`;
+    }
+  }
+
+  const heightHeader = new getHeightHeader();
+
+  function getHeightH() {
+    console.log(heightHeader.height);
+  }
+
+  getHeightH();
+
+  const root=document.documentElement;
+  root.style.setProperty('--top', heightHeader.height);
+});
