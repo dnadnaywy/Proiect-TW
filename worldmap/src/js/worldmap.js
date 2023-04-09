051b5e7162353155d95a1b01d7a42c5c8eeacf8a
Diana Pantiru <pantirudiana14@gmail.com>
2023-04-09 10:30:14 +0300

    simple worldmap

---
+++ worldmap/src/js/worldmap.js
@@ -0,0 +1,25 @@
+const width = 900;
+const height = 600;
+
+const svg = d3.select('#div-map')
+    .append('svg')
+    .attr('width', width)
+    .attr('height', height);
+
+const projection = d3.geoMercator().scale(140).translate([width / 2, height / 1.4]);
+const path = d3.geoPath(projection);
+
+const g = svg.append('g');
+
+d3.json('https://cdn.jsdelivr.net/npm/world-atlas@2/countries-110m.json')
+    .then(data => {
+        const countries = topojson.feature(data, data.objects.countries);
+        g.selectAll('path')
+            .data(countries.features)
+            .enter()
+            .append('path')
+            .attr('class', 'country')
+            .attr("cursor", "pointer")
+            .attr('d', path);
+
+    });
