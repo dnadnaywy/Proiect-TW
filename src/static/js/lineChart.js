5ee5a2ac4d6b9d40dba7ecce0ab52fb0a74586ff
Panainte Diana <panaintediana29@gmail.com>
2023-06-17 16:19:18 +0300

    buttons between charts work
    in method of attack page only tho

--- src/static/js/lineChart.js
+++ src/static/js/lineChart.js
@@ -1,9 +1,9 @@
-const xValues = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
-const yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
-new Chart("canvas1", {
+var xValuesLineChart = [50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150];
+var yValues = [7, 8, 8, 9, 9, 9, 10, 11, 14, 14, 15];
+new Chart("lineChart", {
   type: "line",
   data: {
-    labels: xValues,
+    labels: xValuesLineChart,
     datasets: [{
       fill: false,
       lineTension: 0,
