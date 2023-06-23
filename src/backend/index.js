c85631b43ca086c7369e0044a1ec69c9a932608a
Panainte Diana <panaintediana29@gmail.com>
2023-06-23 21:56:18 +0300

    newsletter email done

--- src/backend/index.js
+++ src/backend/index.js
@@ -6,3 +6,20 @@
 // const pieChart = require('./model/pieChart');
 
 // pieChart.getAllBombings();
+
+// const HTML_TEMPLATE = require("./utils/mail-template.js");
+// const message = "Hi there, your newsletter confirmation for subscribing at <b>bddsolutions</b> website is here.<br><br>Thank you,<br>BDDSolutions Team"
+// const options = {
+//     from: "bddsolutions.tw@gmail.com", // sender address
+//     to: "bddsolutions.tw@gmail.com", // receiver email
+//     subject: "Newsletter Confirmation", // Subject line
+//     text: message,
+//     html: HTML_TEMPLATE(message),
+// }
+
+// const SENDMAIL = require("./utils/emailProvider.js");
+// // send mail with defined transport object and mail options
+// SENDMAIL(options, (info) => {
+//     console.log("Email sent successfully");
+//     console.log("MESSAGE ID: ", info.messageId);
+// });
---
+++ src/backend/utils/emailProvider.js
@@ -0,0 +1,24 @@
+const nodemailer = require("nodemailer");
+
+// create reusable transporter object using the default SMTP transport
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: "bddsolutions.tw@gmail.com",
+    pass: "umcdjhpuhjtcjfop",
+  },
+});
+
+const SENDMAIL = async (mailDetails, callback) => {
+  try {
+    const info = await transporter.sendMail(mailDetails)
+    callback(info);
+  } catch (error) {
+    console.log(error);
+  } 
+};
+
+module.exports = SENDMAIL;
