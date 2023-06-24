62602a86471a9d6a7f3cad320ba8a374c761cc24
brianamaftei <briana.maftei@gmail.com>
2023-06-24 09:32:14 +0300

    Added admin page

--- src/backend/controller/userController.js
+++ src/backend/controller/userController.js
@@ -5,32 +5,28 @@ const config = require('../utils/configuration');
 const {parseJSON, matchJSONProperties} = require('../utils/parseJSON');
 const jwt = require("jsonwebtoken");
 const {deleteCookie} = require("../security/jwtAccesProvider");
+const url = require("url");
 const userController = async (req, res, pool) => {
     const method = req.method;
     const URL = req.url;
 
     if (method === 'GET') {
         if (URL === '/api/users/') {
-            await getAllUsers(res, pool);
+            await getAllUsers(req, res, pool);
         } else {
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Not found');
         }
     } else if (method === 'DELETE') {
 
-        if (URL === '/api/users/deleteAccount') {
-            await deleteUser(req, res, pool);
-        } else if (URL === '/api/users/deleteUserByAdmin') //posibil altceva
-        {
-            // await matchJSONProperties(['username'], req, res);
-            await deleteUserByAdmin(req, res, pool);
+        if (URL.startsWith('/api/users/deleteAccount/')) {
+            const username = URL.split('/')[4];
+            await deleteUser(username, res, pool);
         } else {
             res.writeHead(404, {'Content-Type': 'text/plain'});
             res.end('Not found');
         }
-    }
-    else
-    {
+    } else {
         res.writeHead(404, {'Content-Type': 'text/plain'});
         res.end('Not found');
     }
@@ -39,59 +35,18 @@ const userController = async (req, res, pool) => {
 
 const deleteUser = async (req, res, pool) => {
 
-    const decoded = security.decodedJWT(req);
-
-    if(!decoded){
-        res.end('No cookie, no jwt, no user, no admin no no no');
+    if (!await userModel.deleteUser(req, pool)) {
+        sendMessage(res, {statusCode: 500, status: 'Internal Server Error', message: 'Error deleting user'});
         return;
     }
-
-    await userModel.deactivateUser(decoded['username'], pool);
-
-    security.deleteCookie(res);
-
     sendMessage(res, {statusCode: 200, status: 'OK', message: 'User deleted successfully'});
 }
 
-const deleteUserByAdmin = async (req, res, pool) => {
-    console.log('aaaa'+req+'aaaa');
-
-    if (!security.verifyJWTRole(res, req, config.adminRole)) {
-        return;
-    }
-
-    console.log('aaaa'+req+'aaaa');
-    const user = parseJSON(req);
-    const username = user.username;
-    console.log('aanbbbba'+req+'aajbjobjubaa');
-
-    await userModel.deleteUser(username, pool);
-    if(username===config.adminUsername)
-        security.deleteCookie(res);
-    sendMessage(res, {statusCode: 200, status: 'OK', message: 'User deleted successfully'});
-}
 
 const getAllUsers = async (req, res, pool) => {
-
-    if (security.verifyJWTRole(req, res, config.adminRole) === false) return;
-
     const users = await userModel.getAllUsers(pool);
-    // let userMap = users.map(user => {
-    //     return {
-    //         id: user.id,
-    //         username: user.username,
-    //         firstname: user.firstname,
-    //         lastname: user.lastname,
-    //         email: user.email,
-    //         birthdate: user.birthdate,
-    //         phonenumber: user.phonenumber,
-    //         role: user.role,
-    //         deleted: user.deleted
-    //     }
-    // });
     res.end(JSON.stringify(users));
 }
-
 
 
 module.exports = {userController};
--- src/static/js/users.js
+++ src/static/js/users.js
@@ -1,86 +1,52 @@
-async function displayDataFromDatabase() {
-    const response = await fetch("http://localhost:3000/api/terrorist-cards");
-    try {
-        let container = document.querySelector('#all-cards-content');
+window.onload = function () {
+    fetchAllUsers();
+}
 
-        const jsonData = await response.json();
+let users = [];
+let userToBeDeleted = null;
 
-        let html = '';
-        jsonData.forEach(card => {
-            let htmlSegment = `<div class="card" data-card-id="${card.id}">
-      <p>Country:</p>           <b><p class="country-name">${card.country}</p></b>
-      <p>Region:</p>            <b><p class="region-name">${card.region}</p></b>
-      <p>Attack type:</p>       <b><div class="attack-type-name">${card.attack_type}</div></b>
-      <p>Weapon Type:</p>       <b><p class="weapon-type-name">${card.weapon_type}</p></b>
-      <div class="see-more-details" onclick="seeMoreDetails(event)"><i class="fa-regular fa-eye" title = "Click to see more details!"></i></div> <div class="open-lock"><i class="fa-solid fa-lock-open" onclick="saveCard(event)"></i></div>
-    </div>
-                                  `;
-            html += htmlSegment;
-        });
+async function fetchAllUsers() {
+    const response = await fetch(
+        'http://localhost:3000/api/users/',
+        {method: 'GET'});
+    users = await response.json();
 
-        html += `<div class="pagination">
-    <button class="pagination-pages next-button" current-page="1">Next Page</button>
-  </div>`;
+    const table = document.getElementById('userTable');
 
-        container.innerHTML = html;
+    users.forEach(user => {
+        if (user.role !== 'admin') {
+            let row = table.insertRow();
+            let usernameCell = row.insertCell(0);
+            let emailCell = row.insertCell(1);
+            let deleteCell = row.insertCell(2);
 
-        console.log(jsonData);
-        return await jsonData;
-    } catch (error) {
-        console.log(error);
+            usernameCell.innerHTML = user.username;
+            emailCell.innerHTML = user.email;
+            deleteCell.innerHTML = '<button onclick="confirmDelete(\'' + user.username + '\')">Sterge</button>';
+        }
+    });
+}
+
+function confirmDelete(username) {
+    userToBeDeleted = username;
+    document.getElementById('confirmModal').style.display = "block";
+}
+
+document.getElementById('confirmDelete').onclick = function() {
+    deleteUser(userToBeDeleted);
+    document.getElementById('confirmModal').style.display = "none";
+}
+
+document.getElementById('cancelDelete').onclick = function () {
+    document.getElementById('confirmModal').style.display = "none";
+}
+
+async function deleteUser(username) {
+    const response = await fetch("http://localhost:3000/api/users/deleteAccount/" + username, {method: 'DELETE'});
+
+    if (response.ok) {
+        location.reload();
+    } else {
+        console.error('Failed to delete user');
     }
-}
-
-const seeMoreText = document.querySelectorAll('.see-more-details');
-
-function showAlert(event) {
-    alert('Hello world!');
-}
-
-async function seeMoreDetails(event) {
-    const card = event.target.closest('.card');
-    const cardId = card.dataset.cardId;
-
-    const response = await fetch("http://localhost:3000/api/terrorist-card/" + cardId);
-
-    try {
-        const jsonData = await response.json();
-
-        let detailsContainer = document.querySelector('#see-more-details-container');
-        let cardContainer = document.getElementById('all-cards-content');
-        detailsContainer.style.display = "flex";
-        cardContainer.style.display = "none";
-
-        let html = '';
-
-        jsonData.forEach(attack => {
-            let htmlSegment = `<div class="more-detail-for-card">
-                  <i class="fa-solid fa-xmark" onclick="closeSeeMoreDetails()"></i>
-                  <br><br>
-                  <i class="fa-solid fa-arrow-right"></i>
-                  This attack took place in <b>${attack.country}</b>, <b>${attack.region}</b>. The <b>${attack.group_name}</b> targeted <b>${attack.target}</b>.<br>
-                  <br>
-                  <i class="fa-solid fa-arrow-right"></i> Weapon Type: <b>${attack.weapon_type}</b>. Weapon subtype: <b>${attack.weapon_subtype}.</b><br>
-                  <br>
-                  <i>This is the summary of the attack:</i>
-                  <br><br>
-                  <i class="fa-solid fa-arrow-right"></i> ${attack.summary}
-                </div>`;
-            html += htmlSegment;
-        });
-
-        detailsContainer.innerHTML = html;
-
-        console.log(jsonData);
-        return await jsonData;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-function closeSeeMoreDetails() {
-    let detailsContainer = document.querySelector('#see-more-details-container');
-    let cardContainer = document.getElementById('all-cards-content');
-    detailsContainer.style.display = "none";
-    cardContainer.style.display = "flex";
 }
